c431585bc219617f94e5bf16cc211fb7af32f22e
triadalp <triadalp@gmail.com>
2026-04-15 21:51:40 +0200

    chore: optimize hero images

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -2,10 +2,10 @@ import clsx from "clsx";
 import { useEffect, useState } from "react";
 import { Container } from "../../components/layout/Container/Container";
 import { Button } from "../../components/ui/Button/Button";
-import slide1 from "../../assets/images/slide1.png";
-import slide2 from "../../assets/images/slide2.png";
-import slide3 from "../../assets/images/slide3.png";
-import slide4 from "../../assets/images/slide4.png";
+import slide1 from "../../assets/images/slide1.jpg";
+import slide2 from "../../assets/images/slide2.jpg";
+import slide3 from "../../assets/images/slide3.jpg";
+import slide4 from "../../assets/images/slide4.jpg";
 import style from "./Hero.module.scss";
 
 interface HeroProps {}
