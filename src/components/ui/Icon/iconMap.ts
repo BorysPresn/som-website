1a81435e3e891313b0666a58a13dd68fa159abbc
BorysPresn <boryspresniak@gmail.com>
2026-04-27 20:19:44 +0200

    feat: add Google and Star icons, update icon mapping and types

--- src/components/ui/Icon/iconMap.ts
+++ src/components/ui/Icon/iconMap.ts
@@ -2,7 +2,9 @@ import type { FC, SVGProps } from "react";
 import ArrowRightIcon from "../../../assets/icons/arrow-right.svg?react";
 import ArrowUpRightIcon from "../../../assets/icons/arrow-up-right.svg?react";
 import FacebookIcon from "../../../assets/icons/facebook.svg?react";
+import GoogleIcon from "../../../assets/icons/google-icon-logo.svg?react";
 import InstagramIcon from "../../../assets/icons/instagram.svg?react";
+import StarIcon from "../../../assets/icons/star.svg?react";
 import TikTokIcon from "../../../assets/icons/tik-tok.svg?react";
 import type { IconName } from "../../../types/types";
 
@@ -12,6 +14,8 @@ export const iconMap: Partial<Record<IconName, IconComponent>> = {
   "arrow-right": ArrowRightIcon,
   "arrow-up-right": ArrowUpRightIcon,
   "facebook": FacebookIcon,
+  "google": GoogleIcon,
   "instagram": InstagramIcon,
+  "star": StarIcon,
   "tik-tok": TikTokIcon,
 };
