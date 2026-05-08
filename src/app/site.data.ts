167192aa4df570323a6cd9f2580ab104dd590ecd
BorysPresn <boryspresniak@gmail.com>
2026-05-08 18:50:48 +0200

    fix: temporary update site logo image to high-resolution version to check Git-Hub autodeploy

--- src/app/site.data.ts
+++ src/app/site.data.ts
@@ -1,4 +1,4 @@
-import logo from "../assets/images/logo_web_70x70.webp";
+import logo from "../assets/images/logo_300x300.png";
 import type { SocialMediaIcons } from "../types/types";
 
 export const siteLogo = {
